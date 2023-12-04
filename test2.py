3314ab8741d0e613f4bbe4fcf835f744a0895ba5
shayebuhuiya <2485925@qq.com>
2023-12-04 19:02:20 +0800

    seg

--- test2.py
+++ test2.py
@@ -1,61 +1,53 @@
-# from ultralytics import YOLO
-# import threading
-# import os
-# import cv2
-# import time
-#
-# if __name__ == "__main__":
-#
-#     # model1 = YOLO(r'.\models\sawBlade-seg\best.pt')
-#     model2 = YOLO(r'.\models\sawBladev4\best.pt')
-#
-#     def detect1(dec_image, dec_model):
-#         results = dec_model(dec_image)
-#         print(results)
-#         # res = results[0].plot()
-#         # cv2.namedWindow("pic1", cv2.WINDOW_NORMAL)
-#         # cv2.resizeWindow("pic1", 1000, 800)
-#         # cv2.imshow("pic1", res)
-#         # cv2.waitKey()
-#
-#     def detect2(dec_image, dec_model):
-#         results = dec_model(dec_image)
-#         print(results)
-#         # res = results[0].plot()
-#         # cv2.namedWindow("pic2", cv2.WINDOW_NORMAL)
-#         # cv2.resizeWindow("pic2", 1000, 800)
-#         # cv2.imshow("pic2", res)
-#         # cv2.waitKey()
-#
-#     path = r"E:\sawBlade\all"
-#     paths = os.listdir(path)
-#     for file in paths:
-#         filepath = os.path.join(path, file)
-#         img = cv2.imread(filepath)
-#         res = model2(img)
-#         print(res)
-#         time.sleep(5)
-#
-#         # # t1 = threading.Thread(target=detect1, args=(img, model1))
-#         # t2 = threading.Thread(target=detect2, args=(img, model2))
-#         #
-#         # # t1.start()
-#         # t2.start()
-#         #
-#         # # t1.join()
-#         # t2.join()
-#
-
-
-import cv2
+from ultralytics import YOLO
+import threading
 import os
-from Detect import detect
+import cv2
+import time
+import numpy as np
+
+
+def segImage(image):
+    model = YOLO(r'.\models\sawBlade-seg\best.pt')
+    results = model(img)
+    # res = results[0].plot()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    res = np.zeros_like(gray)
+    for result in results:
+        idx = 0
+        masks = result.masks
+        for mask in masks:
+            print(result.boxes.conf[idx].item())
+            if result.boxes.conf[idx].item() < 0.6:
+                idx += 1
+                continue
+            idx += 1
+            filled_img = np.zeros_like(gray)
+            cv2.fillPoly(filled_img, [np.array(mask.xy, dtype=np.int32)], color=255)
+            res = cv2.bitwise_or(filled_img, res)
+    res = cv2.bitwise_and(res, gray)
 
 if __name__ == "__main__":
+
+    model2 = YOLO(r'.\models\sawBladev4\best.pt')
+
     path = r"E:\sawBlade\all"
     paths = os.listdir(path)
-    img = cv2.imread(os.path.join(path, paths[0]))
-    img = detect(img)
-    cv2.imshow("pic", img)
-    cv2.waitKey()
+    for file in paths:
+        filepath = os.path.join(path, file)
+        img = cv2.imread(filepath)
+        res = model2(img)
+        print(res)
+        time.sleep(5)
+
+        # # t1 = threading.Thread(target=detect1, args=(img, model1))
+        # t2 = threading.Thread(target=detect2, args=(img, model2))
+        #
+        # # t1.start()
+        # t2.start()
+        #
+        # # t1.join()
+        # t2.join()
+
+
+
 
